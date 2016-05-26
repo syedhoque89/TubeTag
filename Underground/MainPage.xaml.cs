6093901cf17d810a2221d82be633b7aa2170a683
syedhoque89 <nadeem.redwanul@hotmail.com>
2016-05-26 10:13:04 +0100

    Added colour specification in line with tfl guideline

--- Underground/MainPage.xaml.cs
+++ Underground/MainPage.xaml.cs
@@ -25,9 +25,11 @@ namespace Underground
 
         private async void IconListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            MyProgressRing.IsActive = true;
             var res = await tflFacade.GetTubeStatusAsync();
             MySplitView.IsPaneOpen = false;
             var selection = IconListBox.SelectedIndex.ToString();
+            MyProgressRing.IsActive = false;
             try
             {
                 switch (selection)
@@ -35,6 +37,9 @@ namespace Underground
                     //Bakerloo line
                     case "0":
                         MyMainGrid.Background = new SolidColorBrush(Colors.Sienna);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[0].name;
                         DetailsTextBlock.Text = res[0].lineStatuses[0].statusSeverityDescription;
                         if (res[0].lineStatuses[0].reason != null)
@@ -49,6 +54,9 @@ namespace Underground
                     //Central line
                     case "1":
                         MyMainGrid.Background = new SolidColorBrush(Colors.DarkRed);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[1].name;
                         DetailsTextBlock.Text = res[1].lineStatuses[0].statusSeverityDescription;
                         if (res[1].lineStatuses[0].reason != null)
@@ -63,6 +71,9 @@ namespace Underground
                     //Circle line
                     case "2":
                         MyMainGrid.Background = new SolidColorBrush(Colors.Gold);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
                         TitleTextBlock.Text = res[2].name;
                         DetailsTextBlock.Text = res[2].lineStatuses[0].statusSeverityDescription;
                         if (res[2].lineStatuses[0].reason != null)
@@ -77,9 +88,12 @@ namespace Underground
                     //District line
                     case "3":
                         MyMainGrid.Background = new SolidColorBrush(Colors.DarkGreen);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[3].name;
                         DetailsTextBlock.Text = res[3].lineStatuses[0].statusSeverityDescription;
-                        if (res[0].lineStatuses[0].reason != null)
+                        if (res[3].lineStatuses[0].reason != null)
                         {
                             ReasonTextBlock.Text = res[3].lineStatuses[0].reason;
                         }
@@ -91,6 +105,9 @@ namespace Underground
                     //DLR
                     case "4":
                         MyMainGrid.Background = new SolidColorBrush(Colors.DeepSkyBlue);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[4].name;
                         DetailsTextBlock.Text = res[4].lineStatuses[0].statusSeverityDescription;
                         if (res[4].lineStatuses[0].reason != null)
@@ -105,6 +122,9 @@ namespace Underground
                     //Hammersmith & City
                     case "5":
                         MyMainGrid.Background = new SolidColorBrush(Colors.LightPink);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
                         TitleTextBlock.Text = res[5].name;
                         DetailsTextBlock.Text = res[5].lineStatuses[0].statusSeverityDescription;
                         if (res[5].lineStatuses[0].reason != null)
@@ -119,6 +139,9 @@ namespace Underground
                     //Jubilee line
                     case "6":
                         MyMainGrid.Background = new SolidColorBrush(Colors.Silver);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[6].name;
                         DetailsTextBlock.Text = res[6].lineStatuses[0].statusSeverityDescription;
                         if (res[6].lineStatuses[0].reason != null)
@@ -133,6 +156,9 @@ namespace Underground
                     //Metropolitan line
                     case "7":
                         MyMainGrid.Background = new SolidColorBrush(Colors.DarkMagenta);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[8].name;
                         DetailsTextBlock.Text = res[8].lineStatuses[0].statusSeverityDescription;
                         if (res[8].lineStatuses[0].reason != null)
@@ -147,7 +173,10 @@ namespace Underground
 
                     //Northern line
                     case "8":
-                        MyMainGrid.Background = new SolidColorBrush(Colors.Black);
+                        MyMainGrid.Background = new SolidColorBrush(Colors.White);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[9].name;
                         DetailsTextBlock.Text = res[9].lineStatuses[0].statusSeverityDescription;
                         if (res[9].lineStatuses[0].reason != null)
@@ -162,6 +191,9 @@ namespace Underground
                     //Overground
                     case "9":
                         MyMainGrid.Background = new SolidColorBrush(Colors.DarkOrange);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[7].name;
                         DetailsTextBlock.Text = res[7].lineStatuses[0].statusSeverityDescription;
                         if (res[7].lineStatuses[0].reason != null)
@@ -176,6 +208,9 @@ namespace Underground
                     //Piccadilly line
                     case "10":
                         MyMainGrid.Background = new SolidColorBrush(Colors.DarkBlue);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[10].name;
                         DetailsTextBlock.Text = res[10].lineStatuses[0].statusSeverityDescription;
                         if (res[10].lineStatuses[0].reason != null)
@@ -190,6 +225,9 @@ namespace Underground
                     //Victoria line
                     case "11":
                         MyMainGrid.Background = new SolidColorBrush(Colors.LightSkyBlue);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.White);
                         TitleTextBlock.Text = res[11].name;
                         DetailsTextBlock.Text = res[11].lineStatuses[0].statusSeverityDescription;
                         if (res[11].lineStatuses[0].reason != null)
@@ -204,6 +242,9 @@ namespace Underground
                     //Waterloo & City line
                     case "12":
                         MyMainGrid.Background = new SolidColorBrush(Colors.PowderBlue);
+                        TitleTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
+                        DetailsTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
+                        ReasonTextBlock.Foreground = new SolidColorBrush(Colors.MidnightBlue);
                         TitleTextBlock.Text = res[12].name;
                         DetailsTextBlock.Text = res[12].lineStatuses[0].statusSeverityDescription;
                         if (res[12].lineStatuses[0].reason != null)
@@ -219,6 +260,10 @@ namespace Underground
             }
             catch
             {
+                TitleTextBlock.Text = "Error!!!";
+                DetailsTextBlock.Text = "Couldn't Retrieve Tube Status..";
+                ReasonTextBlock.Text = "";
+
             }
         }
 
@@ -228,6 +273,12 @@ namespace Underground
         }
         private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
         {
+            
+        }
+
+        private void MainPage_OnLoading(FrameworkElement sender, object args)
+        {
+            
         }
     }
 }
--- Underground/tflFacade.cs
+++ Underground/tflFacade.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -12,20 +13,27 @@ namespace Underground
         {
             //Assemble the url
             //string url = "https://api.tfl.gov.uk/Line/bakerloo,central,circle,district,dlr,hammersmith-city,jubilee,metropolitan,northern,piccadilly,victoria,waterloo-city/Status?detail=true&app_id=849969ba&app_key=743b87c010cef6c8808c0d9a74ba71f1";
-            var url =
-                "https://api.tfl.gov.uk/line/mode/tube,overground,dlr/Status?detail=true&app_id=849969ba&app_key=743b87c010cef6c8808c0d9a74ba71f1";
+            var url =                "https://api.tfl.gov.uk/line/mode/tube,overground,dlr/Status?detail=true&app_id=849969ba&app_key=743b87c010cef6c8808c0d9a74ba71f1";
 
             //Call tfl API
             var http = new HttpClient();
-            var response = await http.GetAsync(url);
+            var response = new HttpResponseMessage();
+            try
+            {
+               response = await http.GetAsync(url);
+            }
+            catch (Exception)
+            {
+                    
+                throw;
+            }
+            
             var jsonMessage = await response.Content.ReadAsStringAsync();
 
 
             //Respons -> string/json -> Deserialize
 
             var result = JsonConvert.DeserializeObject<List<RootArray>>(jsonMessage);
-
-
             return result;
         }
     }
